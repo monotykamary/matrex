836e0122023aaba8dac3c9d73cdd4cde19ce8ea0
Jaremy Creechley <creechley@gmail.com>
2020-01-20 08:30:38 -0700

    small tweaks

--- lib/matrex/algorithms/gradient_descent.ex
+++ lib/matrex/algorithms/gradient_descent.ex
@@ -1,3 +1,25 @@
+# Copyright (C) 2001 and 2002 by Carl Edward Rasmussen. Date 2002-02-13
+#
+#
+# (C) Copyright 1999, 2000 & 2001, Carl Edward Rasmussen
+#
+# Permission is granted for anyone to copy, use, or modify these
+# programs and accompanying documents for purposes of research or
+# education, provided this copyright notice is retained, and note is
+# made of any changes that have been made.
+#
+# These programs and documents are distributed without any warranty,
+# express or implied.  As the programs were written for research
+# purposes only, they have not been tested to the degree that would be
+# advisable in any important application.  All use of these programs is
+# entirely at the user's own risk.
+#
+# [ml-class] Changes Made:
+# 1) Function name and argument specifications
+# 2) Output display
+#
+# [versilov] Changes:
+# 1) Ported to Elixir
 
 defmodule Matrex.Algorithms.GradientDescent do
   alias Matrex.Dashboard
