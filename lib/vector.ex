daa075e15d8e217130ae35d636af66a00ba00696
Jaremy Creechley <creechley@gmail.com>
2020-01-30 16:39:06 -0700

    more vector wrappers

--- lib/vector.ex
+++ lib/vector.ex
@@ -1,7 +1,10 @@
 defmodule Matrex.Vector do
+  import Matrex.Guards
+
   @moduledoc """
   Vector wrappers for Matrex.
   ```
+
 
   """
 
@@ -10,7 +13,24 @@ defmodule Matrex.Vector do
 
   ## Examples
 
-      iex> [1,2,3] |> Matrex.from_list()
+      iex> [1,2,3] |> Vector.new()
+      #Matrex[1×3]
+      ┌                         ┐
+      │     1.0     2.0     3.0 │
+      └                         ┘
+
+  """
+  def new(lst) when is_list(lst) do
+    # Matrex.new( for l <- lst, into: [] do [l] end )
+    Matrex.new( [lst] )
+  end
+
+  @doc """
+  Creates new 1-column matrix (aka vector) from the given list.
+
+  ## Examples
+
+      iex> [1,2,3] |> Vector.from_list()
       #Matrex[1×3]
       ┌                         ┐
       │     1.0     2.0     3.0 │
@@ -18,7 +38,8 @@ defmodule Matrex.Vector do
 
   """
   def from_list(lst) when is_list(lst) do
-    Matrex.new( for l <- lst, into: [] do [l] end )
+    # Matrex.new( for l <- lst, into: [] do [l] end )
+    Matrex.new( [lst] )
   end
 
   @doc """
@@ -26,12 +47,9 @@ defmodule Matrex.Vector do
 
   ## Example
 
-      iex> Matrex.fill(3, 55)
-      #Matrex[4×3]
+      iex> Vector.fill(3, 55)
+      #Matrex[1×3]
       ┌                         ┐
-      │    55.0    55.0    55.0 │
-      │    55.0    55.0    55.0 │
-      │    55.0    55.0    55.0 │
       │    55.0    55.0    55.0 │
       └                         ┘
   """
@@ -45,10 +63,9 @@ defmodule Matrex.Vector do
 
   ## Example
 
-      iex> Matrex.ones(2, 3)
-      #Matrex[2×3]
+      iex> Vector.ones(3)
+      #Matrex[1×3]
       ┌                         ┐
-      │     1.0     1.0     1.0 │
       │     1.0     1.0     1.0 │
       └                         ┘
   """
@@ -62,16 +79,41 @@ defmodule Matrex.Vector do
 
   ## Example
 
-      iex> Matrex.zeros(4,3)
+      iex> Matrex.zeros(3)
       #Matrex[4×3]
       ┌                         ┐
-      │     0.0     0.0     0.0 │
-      │     0.0     0.0     0.0 │
-      │     0.0     0.0     0.0 │
       │     0.0     0.0     0.0 │
       └                         ┘
   """
   @spec zeros(Matrex.index()) :: Matrex.t()
   def zeros(len) when is_integer(len), do: Matrex.zeros(1,len)
+
+  @doc """
+  Set element of matrix at the specified position (one-based) to new value.
+
+  ## Example
+
+      iex> m = Vector.ones(3)
+      #Matrex[3×3]
+      ┌                         ┐
+      │     1.0     1.0     1.0 │
+      └                         ┘
+
+      iex> m = Vector.set(m, 2, 0.0)
+      #Matrex[3×3]
+      ┌                         ┐
+      │     1.0     0.0     1.0 │
+      └                         ┘
+
+      iex> m = Vector.set(m, 2, :neg_inf)
+      #Matrex[3×3]
+      ┌                         ┐
+      │     1.0     -∞      1.0 │
+      └                         ┘
+  """
+  @spec set(Matrex.t(), Matrex.index(), Matrex.element()) :: Matrex.t()
+  def set(vector_data(len, _rest, _data) = m, idx, value)
+      when (is_number(value) or value in [:nan, :inf, :neg_inf]) and idx > 0 and idx <= len,
+      do: Matrex.set(m, 1, idx, value)
 
 end
---
+++ test/vector_basics_test.exs
@@ -0,0 +1,23 @@
+defmodule VectorBasicsTest do
+  use ExUnit.Case, async: true
+
+  alias Matrex.Vector
+
+  test "test new" do
+    vec1 = Vector.new([1, 2, 3])
+    vec2 = Vector.from_list([1, 2, 3])
+    assert vec1 == Matrex.new(" 1 2 3")
+    assert vec2 == Matrex.new(" 1 2 3")
+  end
+
+  test "test zeros" do
+    vec1 = Vector.zeros(3)
+    assert vec1 == Matrex.new("0 0 0")
+  end
+
+  test "test fill" do
+    vec1 = Vector.fill(2, 3.5)
+    assert vec1 == Matrex.new("3.5 3.5")
+  end
+
+end
